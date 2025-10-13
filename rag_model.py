6283fb6f603e228a3150d906cc38a59392d22ed5
Gitlibcode <108735426+Gitlibcode@users.noreply.github.com>
2025-10-13 13:41:10 +0530

    Update rag_model.py

--- rag_model.py
+++ rag_model.py
@@ -79,7 +79,7 @@ Context:
 
 Query: {query}
 
-Answer in one clear sentence with figure if data involved in table and then details description below:"""
+Answer in one clear sentence:"""
 
     url = "https://api.z.ai/api/paas/v4/chat/completions"
     headers = {
